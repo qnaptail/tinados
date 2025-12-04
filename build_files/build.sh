e6fc76fd909e67034027adee15fdd0f3599b2a4f
qnaptail <qnaptail@proton.me>
2025-12-04 04:46:59 +0100

    Niri setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -33,8 +33,23 @@ grep -vE '^#' /usr/local/share/os-template/packages-add | xargs dnf5 -y install 
 grep -vE '^#' /usr/local/share/os-template/packages-remove | xargs dnf5 -y remove
 
 # SWITCH KERNEL TO CACHYOS KERNEL
+# create a shims to bypass kernel install triggering dracut/rpm-ostree
+pushd /usr/lib/kernel/install.d
+mv 05-rpmostree.install 05-rpmostree.install.bak
+mv 50-dracut.install 50-dracut.install.bak
+printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
+printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
+chmod +x  05-rpmostree.install 50-dracut.install
+popd
+
 dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 dnf5 -y install kernel-cachyos
+
+pushd /usr/lib/kernel/install.d
+mv -f 05-rpmostree.install.bak 05-rpmostree.install
+mv -f 50-dracut.install.bak 50-dracut.install
+popd
+
 # Lastly if you use SELinux, you need to enable the necessary policy to be able to load kernel modules.
 setsebool -P domain_kernel_load_modules on
 
