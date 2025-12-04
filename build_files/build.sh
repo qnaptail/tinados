fa41fc884c67267ed54dc69a79f38d6258b1670a
qnaptail <qnaptail@proton.me>
2025-12-04 07:09:22 +0100

    Niri setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,13 +34,13 @@ dnf5 -y copr enable bieszczaders/kernel-cachyos
 
 # SWITCH KERNEL TO CACHYOS KERNEL
 # create a shims to bypass kernel install triggering dracut/rpm-ostree
-pushd /usr/lib/kernel/install.d
-mv 05-rpmostree.install 05-rpmostree.install.bak
-mv 50-dracut.install 50-dracut.install.bak
-printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
-printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
-chmod +x  05-rpmostree.install 50-dracut.install
-popd
+# pushd /usr/lib/kernel/install.d
+# mv 05-rpmostree.install 05-rpmostree.install.bak
+# mv 50-dracut.install 50-dracut.install.bak
+# printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
+# printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
+# chmod +x  05-rpmostree.install 50-dracut.install
+# popd
 
 # dnf5 -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
@@ -50,10 +50,10 @@ dnf5 -y install kernel kernel-core kernel-modules kernel-modules-core kernel-mod
 # grub2-mkconfig -o /boot/grub2/grub.cfg
 
 
-pushd /usr/lib/kernel/install.d
-mv -f 05-rpmostree.install.bak 05-rpmostree.install
-mv -f 50-dracut.install.bak 50-dracut.install
-popd
+# pushd /usr/lib/kernel/install.d
+# mv -f 05-rpmostree.install.bak 05-rpmostree.install
+# mv -f 50-dracut.install.bak 50-dracut.install
+# popd
 
 # Lastly if you use SELinux, you need to enable the necessary policy to be able to load kernel modules.
 setsebool -P domain_kernel_load_modules on
