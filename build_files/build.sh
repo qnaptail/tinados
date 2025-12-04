c36aa8ab2f2a4518ac1118373e9f1f434ed111b2
qnaptail <qnaptail@proton.me>
2025-12-04 16:30:28 +0100

    Niri setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -36,10 +36,10 @@ dnf5 -y copr enable bieszczaders/kernel-cachyos
 # create a shims to bypass kernel install triggering dracut/rpm-ostree
 pushd /usr/lib/kernel/install.d
 mv 05-rpmostree.install 05-rpmostree.install.bak
-#mv 50-dracut.install 50-dracut.install.bak
+mv 50-dracut.install 50-dracut.install.bak
 printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
-#printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
-chmod +x  05-rpmostree.install #50-dracut.install
+printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
+chmod +x  05-rpmostree.install 50-dracut.install
 popd
 
 # dnf5 -y remove --no-autoremove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
@@ -48,8 +48,13 @@ dnf5 -y install kernel-cachyos #kernel-cachyos-devel-matched
 
 pushd /usr/lib/kernel/install.d
 mv -f 05-rpmostree.install.bak 05-rpmostree.install
-#mv -f 50-dracut.install.bak 50-dracut.install
+mv -f 50-dracut.install.bak 50-dracut.install
 popd
+
+# Rebuild initramfs
+QUALIFIED_KERNEL="$(dnf5 repoquery --installed --queryformat='%{evr}.%{arch}' "kernel-cachyos")"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
 
 # Lastly if you use SELinux, you need to enable the necessary policy to be able to load kernel modules.
 setsebool -P domain_kernel_load_modules on
