a3f6c38ffedbb3cf8ffc97e525f9252e6087ef79
qnaptail <qnaptail@proton.me>
2026-05-28 15:08:04 +0200

    fix

---
+++ build/noctalia/build.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+OSNAME="tinados"
+
+
+#######################################################################
+# CONFIGURATION
+#######################################################################
+
+## Copy all config files from rootfs directory to the system
+cp -avf "/ctx/rootfs"/. /
+
+
+##########################################################
+# PACKAGES
+##########################################################
+
+# Save package lists in  /usr/local/share/
+mkdir -p /usr/share/$OSNAME
+mkdir -p /usr/local/share/$OSNAME
+jq -r .packages[] /usr/share/rpm-ostree/treefile.json > /usr/local/share/$OSNAME/packages-base-image
+cp /ctx/build/kde/packages-add /usr/local/share/$OSNAME/packages-add
+cp /ctx/build/kde/packages-remove /usr/local/share/$OSNAME/packages-remove
+chmod  0644 /usr/local/share/$OSNAME/*
+
+# Create /nix store bind mount
+mkdir -p /var/nix
+mkdir -p /nix
+# mount --bind /var/nix /nix
+
+
+# Add Terra repository (needed for Noctalia)
+dnf5 install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+
+# Install listed packages
+grep -vE '^#' /usr/local/share/$OSNAME/packages-add | xargs dnf5 -y install --allowerasing
+
+# Remove unnecessary packages
+grep -vE '^#' /usr/local/share/$OSNAME/packages-remove | xargs dnf5 -y remove
+
+# Cleanup
+dnf5 -y autoremove
+
+
+#######################################################################
+# SERVICES
+#######################################################################
+
+systemctl set-default graphical.target
+
+system_services=(
+#     chronyd.service
+    firewalld.service
+    nix.mount
+    nix-daemon.service
+    systemd-timesyncd.service
+    systemd-resolved.service
+    systemd-homed.service
+#     virtqemud.service
+#     zcfan.service # Thinkpad fan control
+)
+
+user_services=(
+    podman.socket
+)
+
+mask_services=(
+    logrotate.timer
+    logrotate.service
+    rpm-ostree-countme.timer
+    rpm-ostree-countme.service
+    systemd-remount-fs.service
+    flatpak-add-fedora-repos.service
+    NetworkManager-wait-online.service
+)
+
+systemctl enable "${system_services[@]}"
+systemctl --global enable "${user_services[@]}"
+systemctl mask "${mask_services[@]}"
+
+
+#######################################################################
+# TROUBLESHOOTING
+#######################################################################
+
